823dcb37ff0411b02ea7b046c8b9729c807496e0
kashif9911 <93089705+kashif9911@users.noreply.github.com>
2021-11-06 16:06:45 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,7 @@
 
     $(function () {
         element.typed({
-            strings: ["Web Designer.", "Graphic Designer."],
+            strings: ["Web Layout Designer.", "Graphic Designer.", "3D Modeler"],
             typeSpeed: 100,
             loop: true,
             autoplay: true,
